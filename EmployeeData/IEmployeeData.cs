9d4394a53de852b9de06fc7d680d127c7c87ba3b
mwvstudio@outlook.com <mwvstudio@outlook.com>
2022-01-13 21:20:49 +0100

    Add project files.

---
+++ EmployeeData/IEmployeeData.cs
@@ -0,0 +1,17 @@
+﻿using BasicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicApi.EmployeeData
+{
+    public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+        Employee GetEmployee(Guid id);
+        Employee AddEmployee(Employee employee);
+        void DeleteEmployee(Employee employee);
+        Employee EditEmployee(Employee employee);
+    }
+}
